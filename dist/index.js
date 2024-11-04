e10d4fce6187616c77f1a7bb6be869be4eb522ba
Haru <thuthuy.8610@gmail.com>
2024-11-04 20:37:49 +0700

    create logic for users to guess a number between 1 and 10. The number is generated at the beginning

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,28 @@
+const numberInput = document.getElementById("numberInput");
+const guessForm = document.getElementById("guessForm");
+const appMessage = document.getElementById("appMessage");
+const numberChosen = getRandomNumber() // Generate a random number only once. If I put it in the guessForm, it'll get updated every time users submit something.
+let count = 0;
+
+guessForm.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const guessInput = Number(numberInput.value);
+    count ++;
+
+    if (numberChosen > guessInput) {
+        appMessage.textContent = `The number is greater than ${guessInput}`;
+    }
+    else if (numberChosen < guessInput) {
+        appMessage.textContent = `The number is less than ${guessInput}`;
+    }   
+    else {
+        appMessage.textContent = `Well done! It took you ${count} attempts to guess this number`;
+    }           
+})
+
+function getRandomNumber() {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    return randomNumber;
+}
 
